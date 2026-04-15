50c360030093a06a53da4896336071fc23ab9524
souyash <souyash.y@gmail.com>
2026-04-15 10:04:51 +0530

    implemented unionfs_truncate for file size adjustment

--- ops_write.c
+++ ops_write.c
@@ -1,5 +1,6 @@
 #include "ops_write.h"
 #include "path_utils.h"
+#include "cow.h"
 #include "state.h"
 #include <fcntl.h>
 #include <unistd.h>
@@ -77,5 +78,23 @@ int unionfs_rmdir(const char *path) {
     char upper_path[PATH_MAX];
     make_path(state->upper_dir, path, upper_path);
     if (rmdir(upper_path) < 0) return -errno;
+    return 0;
+}
+
+int unionfs_truncate(const char *path, off_t size, struct fuse_file_info *fi) {
+    (void) fi;
+
+    if (!is_in_upper(path)) {
+        int res = cow_copy(path);
+        if (res < 0) return res;
+    }
+
+    struct mini_unionfs_state *state = UNIONFS_DATA;
+    char upper_path[PATH_MAX];
+    make_path(state->upper_dir, path, upper_path);
+
+    if (truncate(upper_path, size) < 0)
+        return -errno;
+
     return 0;
 }
